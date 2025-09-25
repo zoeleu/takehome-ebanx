ed2b97b0d4e95f889cf88f9dc05235f393151a12
Zoe Leullier <git@zoe.dev.br>
2025-09-25 15:32:41 -0300

    Implement transfer event

--- src/routes/events/transfer.ts
+++ src/routes/events/transfer.ts
@@ -1,6 +1,30 @@
 import { Context } from "hono";
 import { TransferEvent } from "./types";
+import { users } from "@/index";
+import { User } from "@/lib/user";
 
 export function transferHandler(c: Context, body: TransferEvent): Response {
-  return c.text("Not implemented");
+  const { origin, destination, amount } = body;
+
+  const originUser = users.get(origin);
+
+  if (!originUser) {
+    return c.json(0, 404);
+  }
+
+  let destinationUser = users.get(destination);
+
+  if (!destinationUser) {
+    destinationUser = new User(destination)
+    users.set(destination, destinationUser);
+  }
+
+  try {
+    originUser.transfer(amount, destinationUser);
+
+    return c.json({ origin: originUser.serialize(), destination: destinationUser.serialize() }, 201);
+  } catch (error) {
+    // Insufficient funds
+    return c.json({ origin: originUser.serialize(), destination: destinationUser.serialize() }, 400);
+  }
 }
