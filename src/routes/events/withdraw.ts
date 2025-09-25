d0e997d6310142a16ff01c90f0b92a5fd5c4a89b
Zoe Leullier <git@zoe.dev.br>
2025-09-25 15:34:14 -0300

    Implement withdrawr event

--- src/routes/events/withdraw.ts
+++ src/routes/events/withdraw.ts
@@ -1,6 +1,22 @@
 import { Context } from "hono";
 import { WithdrawEvent } from "./types";
+import { users } from "@/index";
 
 export function withdrawHandler(c: Context, body: WithdrawEvent): Response {
-  return c.text("Not implemented");
+  const { origin, amount } = body;
+
+  const user = users.get(origin);
+
+  if (!user) {
+    return c.json(0, 404);
+  }
+
+  try {
+    user.withdraw(amount);
+
+    return c.json({ origin: user.serialize() }, 201);
+  } catch (error) {
+    // Insufficient funds
+    return c.json({ origin: user.serialize() }, 400);
+  }
 }
