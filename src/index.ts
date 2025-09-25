c5f99e0e2ede8665e2ab44fb3dad5616c38c256f
Zoe Leullier <git@zoe.dev.br>
2025-09-25 14:58:13 -0300

    Implement user balance endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from 'hono'
+import { User } from './lib/user'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const users = new Map<string, User>()
+
+app.get('/balance', async (c) => {
+  const userId = c.req.query('account_id')
+
+  if (!userId) {
+    return c.json(0, 404);
+  }
+
+  const user = users.get(userId);
+
+  if (!user) {
+    return c.json(0, 404);
+  }
+
+  return c.json(user.getBalance());
+});
 
 export default app
